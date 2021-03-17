97475f2048e45b655e2c30672008198f3485fec9
Sirajudheen Mohamed Ali <sirajudheen.mohamed.ali@sap.com>
2021-03-18 02:41:18 +0530

    modifications to ruby notes

--- ruby/examples/metaclasses/call.rb
+++ ruby/examples/metaclasses/call.rb
@@ -1,29 +1,27 @@
 # based on https://viewsourcecode.org/why/hacking/seeingMetaclassesClearly.html
+
 require 'yaml'
 require_relative 'metaid'
 require_relative 'truck'
 
 m = MailTruck.new( "Harold", ['12 Corrigan Way', '23 Antler Ave'] )
 
-
-puts "o = Object.new: #{o = Object.new}"
-puts "o.class: #{o.class}"
-puts "Class.superclass.superclass: #{Class.superclass.superclass}"
-puts "Object.class: #{Object.class}"
+puts "o = Object.new: #{o = Object.new} ", ""
+puts "o.class: #{o.class}",""
+puts "Class.superclass.superclass: #{Class.superclass.superclass}", ""
+puts "Object.class: #{Object.class}", ""
 puts "Object.superclass: #{Object.superclass}"
 
 
 
 m = MailTruck.new( "Harold", ['12 Corrigan Way', '23 Antler Ave'] )
-
-puts "m.class: #{m.class}"
-puts "m.instance_variable_set( \"@speed\", 45 ): #{m.instance_variable_set( '@speed', 45 )}"
-puts "m.driver: #{m.driver}"
-puts "m.object_id: #{m.object_id}"
-puts "MailTruck.object_id: #{MailTruck.object_id}"
-
-puts "m.metaclass.class: #{m.metaclass.class}"
-puts "m.metaclass.superclass: #{m.metaclass.superclass}"
+puts "m.class: #{m.class}", ""
+puts "m.instance_variable_set( \"@speed\", 45 ): #{m.instance_variable_set( '@speed', 45 )}", ""
+puts "m.driver: #{m.driver}", ""
+puts "m.object_id: #{m.object_id}", ""
+puts "MailTruck.object_id: #{MailTruck.object_id}", ""
+puts "m.metaclass.class: #{m.metaclass.class}", ""
+puts "m.metaclass.superclass: #{m.metaclass.superclass}", ""
 puts "m.metaclass.instance_methods: #{m.metaclass.instance_methods}"
 
 class << m
@@ -31,20 +29,18 @@ class << m
     ['@driver', '@route']
   end
 end
-
-puts "m.singleton_methods: #{m.singleton_methods}"
-puts "m.metaclass.instance_methods: #{m.metaclass.instance_methods}"
-puts "YAML::dump m: #{YAML::dump m}"
-
-puts "m.class: #{m.class}"
-puts "m.class.instance_methods: #{m.class.instance_methods}"
-puts "m.inspect: #{m.inspect}"
-puts "m.metaclass: #{m.metaclass}"
+puts "m.singleton_methods: #{m.singleton_methods}", ""
+puts "m.metaclass.instance_methods: #{m.metaclass.instance_methods}", ""
+puts "YAML::dump m: #{YAML::dump m}", ""
+puts "m.class: #{m.class}", ""
+puts "m.class.instance_methods: #{m.class.instance_methods}", ""
+puts "m.inspect: #{m.inspect}", ""
+puts "m.metaclass: #{m.metaclass}", ""
 # puts "m.metaclass.class: #{m.metaclass.class}"
-puts "m.metaclass.superclass: #{m.metaclass.superclass}"
+puts "m.metaclass.superclass: #{m.metaclass.superclass}", ""
 # puts "m.metaclass.superclass.class: #{m.metaclass.superclass.class}"
-puts "m.metaclass.metaclass: #{m.metaclass.metaclass}"
-puts "m.metaclass.metaclass.metaclass: #{m.metaclass.metaclass.metaclass}"
+puts "m.metaclass.metaclass: #{m.metaclass.metaclass}", ""
+puts "m.metaclass.metaclass.metaclass: #{m.metaclass.metaclass.metaclass}", ""
 
 m.meta_eval do
   self.meta_eval do
@@ -54,8 +50,7 @@ m.meta_eval do
     end
   end
 end
-
-puts "m.metaclass.metaclass.singleton_methods #{m.metaclass.metaclass.singleton_methods}"
+puts "m.metaclass.metaclass.singleton_methods #{m.metaclass.metaclass.singleton_methods}", ""
 puts "m.metaclass.metaclass.metaclass.singleton_methods #{m.metaclass.metaclass.metaclass.singleton_methods}"
 
 
--- ruby/examples/metaclasses/truck.rb
+++ ruby/examples/metaclasses/truck.rb
@@ -1,8 +1,49 @@
 # based on https://viewsourcecode.org/why/hacking/seeingMetaclassesClearly.html
-require_relative 'metaid'
+# require_relative 'metaid'
 class MailTruck
   attr_accessor :driver, :route
   def initialize( driver, route )
     @driver, @route = driver, route
   end
+
+  @@trucks = [] # class variable
+  def MailTruck.addTruck( truck )
+    @@trucks << truck
+  end
+
+  def say_hi
+    puts "Hi, I'm one of #{@@trucks.length} trucks!"
+  end
+
+  def MailTruck.display
+    puts "@@trucks: #{@@trucks}"
+    @@trucks.each do |t|
+      puts "truck: #{t}"
+    end
+  end
+
+end
+
+mt = MailTruck.new( "Harold", ['12 Corrigan Way', '23 Antler Ave'] )
+MailTruck.addTruck("Royal Mail Truck")
+MailTruck.addTruck("DHL Truck")
+MailTruck.addTruck("Indian Post Truck")
+MailTruck.display
+mt.display
+mt.say_hi
+MailTruck.instance_eval do
+  puts "(instance_eval) self.name : #{self.name}"
+  puts "(instance_eval) self.display : #{self.display}"
+end
+
+puts "mt.class.inspect: #{mt.class.inspect}"
+puts "mt.class.singleton_methods: #{mt.class.singleton_methods}"
+# puts "mt.class.instance_methods: #{mt.class.instance_methods}"
+puts "mt.class.hash: #{mt.class.hash}"
+
+
+
+mt.class.instance_methods.each do |method|
+  fullmethod = "mt.class.#{method}"
+  puts "#{fullmethod}"
 end
