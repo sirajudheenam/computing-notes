5830cde4e13b0dbe91eaa2389be722ea669f3139
Sirajudheen Mohamed Ali <sirajudheenam@gmail.com>
2024-05-25 22:45:14 +0200

    add javascript folder

---
+++ javascript/index.js
@@ -0,0 +1,49 @@
+// alert("Hello Mumtaz!");
+
+// Variables
+var myName = "Mumtaz"; // String
+var myAge = 25; // Number
+var myHeight = 5.8; // Number
+
+console.log("myName:", myName);
+console.log("myAge:", myAge);
+console.log("myHeight:", myHeight);
+
+// Change variable
+myName = "Mumtaz Ayaz";
+console.log("myName after changing:", myName);
+
+// Constants
+
+const myHusband = "Siraj";
+console.log("myHusband:", myHusband);
+// myHusband = "Ayaz";
+// console.log("myHusband:", myHusband);
+
+// Data Types
+
+const myData = {
+    degree: "MA",
+    city: "Bhopal",
+    state: "MP",
+};
+
+console.log("myData:", myData);
+
+// Operators
+
+// Control Structures
+
+// Functions
+
+function displayData() {
+    document.getElementById("txtAreaDataDisplay").innerHTML = myData;
+    console.log("myData is displayed on textArea:", myData);
+}
+// Objects
+
+// Arrays
+
+// Events
+
+// Inheritance
